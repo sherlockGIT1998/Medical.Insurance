5ca3b893bc2adbce529505a726808a34e865aaa8
carbha01234 <charderajnee20@gmail.com>
2024-10-02 00:55:12 +0530

    all files are added...

---
+++ config.py
@@ -0,0 +1,3 @@
+MODEL_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical.Insurance\Project_app\Medical_Insurance.pkl'
+
+JSON_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical.Insurance\Project_app\Medical_Insurance.json'
